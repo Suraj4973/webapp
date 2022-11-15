e5fdc70a47e3d691aaa70def32b01e555a33d47e
Suraj4973 <hydrasonic03@gmail.com>
2022-11-15 12:45:33 +0530

    Webapp test

---
+++ lasoon/src/main/java/com/kmit/lasoon/App.java
@@ -0,0 +1,13 @@
+package com.kmit.lasoon;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
